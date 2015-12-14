9d80ebb27594e033dc31e7ad05f697ede96e8c1f
Dave Gonzalez <dgonzalez42@gatech.edu>
2015-12-14 00:13:50 -0600

    Initial commit.  Save to file, not console.

---
+++ src/logging/example1/HelloWorld.java
@@ -0,0 +1,63 @@
+package logging.example1;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.SimpleFormatter;
+
+public class HelloWorld 
+{
+	private String theMessage;
+	private static Logger theLogger = Logger.getLogger(HelloWorld.class.getName());
+
+	public static void main(String[] args) 
+	{
+		Handler[] handlers = Logger.getLogger( "" ).getHandlers();
+		Handler handler = null;
+		
+		for (int index = 0; index < handlers.length; index++) 
+		{
+			handlers[index].setLevel( Level.FINE );
+		}
+		
+		
+		try 
+		{
+			handler = new FileHandler("test.log");
+		} 
+		catch (SecurityException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		handler.setFormatter(new SimpleFormatter());
+		theLogger.addHandler(handler);
+		theLogger.setLevel(Level.FINE);
+		theLogger.setUseParentHandlers(false);
+		
+		HelloWorld hello = new HelloWorld("Hello world!");
+		hello.sayHello();
+	}
+
+	public HelloWorld(String message) 
+	{
+		theMessage = message;
+	}
+
+	public void sayHello() 
+	{
+		theLogger.fine("Hello logging!");
+		System.err.println(theMessage);
+		System.out.println(theMessage);
+		theLogger.fine("Message #2");
+	}
+}
